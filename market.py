1bd40851dfd00743acd4a9d7580622d5479e8d41
altayjabbar <jabbarofaltay@gmail.com>
2024-09-08 00:36:45 +0400

    02 - Styling and Templates

--- market.py
+++ market.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, BAKU!</h1>"
+@app.route("/home")
+
+def home_page():
+    return render_template('home.html')
 
 @app.route('/about/<altay>')
 def about_page(altay):
